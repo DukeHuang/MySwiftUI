6fa8ea646bc081c16669c7f0ac08d17dfdd97fc1
huang.yongyou <huang.yongyou@westone.com.cn>
2024-04-02 15:03:50 +0800

    Initial Commit

---
+++ MySwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MySwiftUI
+//
+//  Created by yongyou on 2024/4/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
